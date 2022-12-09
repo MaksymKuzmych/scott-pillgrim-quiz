4bd71b7792201c2d10afcd91fa54aa9bd6f0c362
Maksym Kuzmych <kuzmichmaksym1997@gmail.com>
2022-12-09 19:01:43 +0200

    feat: rewrite appView to ts

---
+++ migration-newip-to-ts/src/components/view/appView.ts
@@ -0,0 +1,25 @@
+import News from './news/news';
+import Sources from './sources/sources';
+import {New, News as NewsInterface, Source, Sources as SourcesInterface} from '../../types/index'
+
+export class AppView {
+    private news: News;
+    private sources: Sources;
+
+    constructor() {
+        this.news = new News();
+        this.sources = new Sources();
+    }
+
+    drawNews(data: NewsInterface) {
+        const values: Array<New> = data?.articles ? data?.articles : [];
+        this.news.draw(values);
+    }
+
+    drawSources(data: SourcesInterface) {
+        const values: Array<Source> = data?.sources ? data?.sources : [];
+        this.sources.draw(values);
+    }
+}
+
+export default AppView;

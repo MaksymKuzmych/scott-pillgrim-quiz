017dee222b787ba296baab00794049b55b99558b
Maksym Kuzmych <kuzmichmaksym1997@gmail.com>
2022-12-09 14:00:09 +0200

    feat: add interfaces for news

---
+++ migration-newip-to-ts/src/types/index.ts
@@ -0,0 +1,19 @@
+export interface New {
+    source: {
+        id: string;
+        name: string;
+    };
+    author: string;
+    title: string;
+    description: string;
+    url: string;
+    urlToImage: string;
+    publishedAt: string;
+    content: string;
+}
+
+export interface News {
+    status: string;
+    totalResults: number;
+    articles: Array<New>;
+}
